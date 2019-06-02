5c0b45ef0290df0fc61b47c02cc237e1f0003095
Omhet <omhetehmo@yandex.ru>
2019-06-02 18:40:01 +0400

    Updates

---
+++ precache-manifest.0dd0625f965694c264ea6816a05896c8.esm.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f2c440d96f85537aed71ab2f0b927fb",
+    "url": "/cookasassets\\icon.png"
+  },
+  {
+    "revision": "f636b1069fecfd602f13",
+    "url": "/cookasbundle.78cc1.css"
+  },
+  {
+    "revision": "9f90011d4d1b9f5adcaf3d8b0fd50902",
+    "url": "/cookasbundle.ad95b.esm.js"
+  },
+  {
+    "revision": "06d72b65821dc70f2e01690f65c99764",
+    "url": "/cookasindex.html"
+  },
+  {
+    "revision": "09854029d406e0f49a728b42430f6a23",
+    "url": "/cookaspolyfills.ffb25.esm.js"
+  }
+]);
---
+++ precache-manifest.bf93498ed686874cd6d041f484f2fc26.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f2c440d96f85537aed71ab2f0b927fb",
+    "url": "/cookasassets\\icon.png"
+  },
+  {
+    "revision": "f636b1069fecfd602f13",
+    "url": "/cookasbundle.78cc1.css"
+  },
+  {
+    "revision": "f636b1069fecfd602f13",
+    "url": "/cookasbundle.f636b.js"
+  },
+  {
+    "revision": "06d72b65821dc70f2e01690f65c99764",
+    "url": "/cookasindex.html"
+  },
+  {
+    "revision": "1f58c278fa1432f79792",
+    "url": "/cookaspolyfills.1f58c.js"
+  },
+  {
+    "revision": "13b82ed03d648451c57be206f75f0216",
+    "url": "/cookassw-debug.js"
+  },
+  {
+    "revision": "1a2a8e50e7f4e14975796d8816c745ac",
+    "url": "/cookassw-esm.js"
+  },
+  {
+    "revision": "7b806584c5458b802154f0f7fe24ca9c",
+    "url": "/cookassw.js"
+  }
+]);
