5d15217bb3e5aea3d29f1deca206671b972e1de3
Omhet <omhetehmo@yandex.ru>
2019-06-02 18:31:20 +0400

    Updates

---
+++ precache-manifest.6bc31dec2a60f01fbafb70e5257db439.esm.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f2c440d96f85537aed71ab2f0b927fb",
+    "url": "/assets\\icon.png"
+  },
+  {
+    "revision": "2a5209634451d4e8dd5b",
+    "url": "/bundle.78cc1.css"
+  },
+  {
+    "revision": "3abc0472e6ee87762309ace4c54ec0dc",
+    "url": "/bundle.eb961.esm.js"
+  },
+  {
+    "revision": "5b4d799eb759432583ff961c3748ee0d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d521c86904bf2dade7d0f5ed07797baf",
+    "url": "/polyfills.fe797.esm.js"
+  }
+]);
