de8cef227c7331539e5c5c4bcd8bb9ab0025e493
Vladimir Ivanov <vlivanov@aligntech.com>
2019-07-19 11:23:00 +0300

    Updates

---
+++ precache-manifest.306d75917ae3e03ec35769c6354356bb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf3fdf7af60a294d6d3f48cb7ad82488",
+    "url": "/cookasassets/icon.png"
+  },
+  {
+    "revision": "3427416a2e79d34f0e8b",
+    "url": "/cookasbundle.34274.js"
+  },
+  {
+    "revision": "3427416a2e79d34f0e8b",
+    "url": "/cookasbundle.dca85.css"
+  },
+  {
+    "revision": "053afe6f08a16d9f90e82bac4b156aad",
+    "url": "/cookasindex.html"
+  },
+  {
+    "revision": "764d84563b7cd1ab9986",
+    "url": "/cookaspolyfills.764d8.js"
+  },
+  {
+    "revision": "13b82ed03d648451c57be206f75f0216",
+    "url": "/cookassw-debug.js"
+  },
+  {
+    "revision": "f21eb868bb750335c0445170da09d560",
+    "url": "/cookassw-esm.js"
+  },
+  {
+    "revision": "172622f83469c602be0f3bb6c665341a",
+    "url": "/cookassw.js"
+  }
+]);
